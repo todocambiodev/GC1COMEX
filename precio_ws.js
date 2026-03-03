b959549dcb37b82245d46c9fee373742dd0d66b2
todocambiodev <todocambiodivisa@gmail.com>
2026-03-03 10:32:36 -0500

    Update precio_ws.js

--- precio_ws.js
+++ precio_ws.js
@@ -158,7 +158,7 @@ let sopActuales = []
 let resActuales = []
 const factor = 1.00126
 const minutosParaRecargarSR = 1
-const cicloFinal = 54
+const cicloFinal = 18
 let cicloActual = 0
 let precio = ""
 // ---------------------
